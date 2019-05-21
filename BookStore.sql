dcb70357523d813f8c712b365c6de5e9c2cdd1da
Yandrick Grima <yandrick.alpha.grima@gmail.com>
2019-05-21 15:50:20 +0200

    added database dump

--- BookStore.sql
+++ BookStore.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2019 at 05:45 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: May 21, 2019 at 03:49 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,97 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `BookStore`
+-- Database: `bookstore`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `book_id` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`book_id`, `user_id`, `price`) VALUES
+('Jc9MaZrG3j8C', 8, 26);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `user_id` int(4) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`user_id`, `image`) VALUES
+(1, 'uploads/DavidMitchell.png'),
+(10, './images/image-not-available.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `user_id` int(4) NOT NULL,
+  `log_message` varchar(255) NOT NULL,
+  `log_time` timestamp(2) NOT NULL DEFAULT CURRENT_TIMESTAMP(2) ON UPDATE CURRENT_TIMESTAMP(2)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `log`
+--
+
+INSERT INTO `log` (`user_id`, `log_message`, `log_time`) VALUES
+(1, 'user logged in', '2019-05-21 13:08:08.00'),
+(1, 'user logged out', '2019-05-21 13:08:09.00'),
+(8, 'user logged in', '2019-05-21 13:08:29.00'),
+(8, 'user logged out', '2019-05-21 13:08:31.00'),
+(9, 'user logged in', '2019-05-21 13:09:24.00'),
+(9, 'user logged out', '2019-05-21 13:09:26.00'),
+(10, 'user logged in', '2019-05-21 13:09:32.00'),
+(10, 'user logged out', '2019-05-21 13:09:33.00'),
+(8, 'user logged in', '2019-05-21 13:10:13.00'),
+(8, 'user logged in', '2019-05-21 13:16:35.00'),
+(8, 'user logged in', '2019-05-21 13:17:51.00'),
+(8, 'user logged out', '2019-05-21 13:43:37.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userbooks`
+--
+
+CREATE TABLE `userbooks` (
+  `user_id` int(11) NOT NULL,
+  `book_id` varchar(50) NOT NULL,
+  `price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userbooks`
+--
+
+INSERT INTO `userbooks` (`user_id`, `book_id`, `price`) VALUES
+(8, 'ukNtSB5CIf0C', 19),
+(9, 'SqMNAQAAMAAJ', 27),
+(8, 'SqMNAQAAMAAJ', 13);
 
 -- --------------------------------------------------------
 
@@ -34,27 +123,55 @@ CREATE TABLE `users` (
   `surname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `isAdmin` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `surname`, `email`, `username`, `password`) VALUES
-(8, 'yan', 'grima', '123@gg.com', 'grimm', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+INSERT INTO `users` (`id`, `name`, `surname`, `email`, `username`, `password`, `isAdmin`) VALUES
+(1, 'yandrick', 'grima', 'yand@gmail.com', 'grimm123', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1),
+(8, 'wendigo', 'sant', 'boithisworks@gmail.com', 'grimm', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 0),
+(9, 'Yandrick', 'Grima', 'yandrick.alpha.grima@gmail.com', 'admin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1),
+(10, 'This is a test account', 'image not available', 'image@not.com', 'TestAccount', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `userbooks`
+--
+ALTER TABLE `userbooks`
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -64,7 +181,35 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `log`
+--
+ALTER TABLE `log`
+  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `userbooks`
+--
+ALTER TABLE `userbooks`
+  ADD CONSTRAINT `userbooks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
